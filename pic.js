c667f5f5f45396836efeadaaf328a9995e910681
Dylan <dylangohn99@gmail.com>
2020-02-25 19:32:35 -0700

    Testing forms

--- pic.js
+++ pic.js
@@ -1,6 +1,16 @@
 
-
+var loot= true;
 function wall(){
-    alert("test")
+    loot =false;
+    if(loot == false)
+    {
+        window.location.replace("pic2.html");
+    }
+    document.getElementsByName("exit").style.backgroundColor = "white"
+}
+function start(){
+    document.getElementsByClassName("wall").style.backgroundColor = "black"
+    document.getElementsByClassName("exit").style.backgroundColor = "gold"
+    
 }
 

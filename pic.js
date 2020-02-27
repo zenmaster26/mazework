57ef5ff7424c6ee7348ba20bcfd57dc265710e60
Dylan <dylangohn99@gmail.com>
2020-02-26 17:26:50 -0700

    Functioning Maze V.0.1

--- pic.js
+++ pic.js
@@ -1,16 +1,31 @@
-
+var lives = 3
 var loot= true;
 function wall(){
-    loot =false;
-    if(loot == false)
-    {
-        window.location.replace("pic2.html");
-    }
-    document.getElementsByName("exit").style.backgroundColor = "white"
+        var b = document.getElementById("exit").style.visibility = "hidden";
+        
+        var x = document.querySelectorAll(".wall");
+        var i;  
+        for(i = 0; i <  x.length; i++){
+                x[i].style.visibility = "hidden";
+                
+            
+            }
+            lives -= 1;
+            console.log(lives)
+            if(lives == 0){
+                window.location.replace("pic2.html")
+            }
 }
+
 function start(){
-    document.getElementsByClassName("wall").style.backgroundColor = "black"
-    document.getElementsByClassName("exit").style.backgroundColor = "gold"
+    
+    document.getElementById("exit").style.visibility = "visible"
+    
+    var x = document.querySelectorAll(".wall");
+    var i;  
+    for(i = 0; i <  x.length; i++){
+            x[i].style.visibility = "visible";
+        }
     
 }
 

f957666590a8b307e35f03ccc899bca220ffe5cb
Dylan <dylangohn99@gmail.com>
2020-03-05 18:39:06 -0700

    Added in images for the life counter
    I <3 you, literally. The things are hearts now.

--- pic.js
+++ pic.js
@@ -1,7 +1,7 @@
 var score = 0
 var lives = 3
 var sword = false
-document.getElementById("lives").innerHTML =  lives + " Lives Left"
+document.getElementById("lives").innerHTML = "<img id='life' src=images/HeartBeating.gif> <img id='life' src=images/HeartBeating.gif> <img id='life' src=images/HeartBeating.gif>"
 function key1(){
     document.getElementById("keyred").style.visibility = "hidden"
     document.getElementById("doorred").style.visibility = "hidden"
@@ -23,9 +23,18 @@ function wall(){
             
             }
             lives -= 1;
-            document.getElementById("lives").innerHTML = lives + " lives"
-            if(lives == 0){
-                window.location.replace("pic2.html")
+            
+            if(lives == 3){
+                document.getElementById("lives").innerHTML = "<img id='life' src=images/HeartBeating.gif> <img id='life' src=images/HeartBeating.gif> <img id='life' src=images/HeartBeating.gif>"
+            }
+            else if(lives == 2){
+              document.getElementById("lives").innerHTML = "<img id='life' src=images/HeartBeating.gif> <img id='life' src=images/HeartBeating.gif>"
+            }
+            else if(lives == 1){
+              document.getElementById("lives").innerHTML = "<img id='life' src=images/HeartBeating.gif>"
+            }
+            else{
+              window.location.replace("pic2.html")
             }
             var c = document.querySelectorAll(".point");
         var i;  
@@ -148,127 +157,99 @@ function win(){
     document.getElementById("winscreen").innerHTML = "Your score is: " + score
 }
 
-window.onload = () => {
-    startSetTimeoutAnimation();
-    startSetTimeoutAnimation2();
-    vObstacle1();
-    vObstacle2();
-    vObstacle3();
-  };
-  // VERTICAL ANIMATION
-  function startSetTimeoutAnimation() {
-    const refreshRate = 1000 / 60;
-    const maxXPosition = 130;
-    let rect = document.getElementById('rect0');
-    let speedX = 1;
-    let positionX = 0;
-    document.getElementById('rect0').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionX = positionX + speedX;
-      if (positionX > maxXPosition || positionX < 0) {
-        speedX = speedX * (-1);
-      }
-      rect.style.left = positionX + 'px';
-    }, refreshRate);
-  }
-  // HORIZONTAL ANIMATION
-    function startSetTimeoutAnimation2() {
-    const refreshRate = 1000 / 60;
-    const maxYPosition = 195;
-    let rect = document.getElementById('rect1');
-    let speedY = 0.7;
-    let positionY = 0;
-    document.getElementById('rect1').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionY = positionY + speedY;
-      if (positionY > maxYPosition || positionY < 0) {
-        speedY = speedY * (-1);
-      }
-      rect.style.top = positionY + 'px';
-    }, refreshRate);
-  }
-  //LEVEL 1
-  function vObstacle1() {
-    const refreshRate = 1000 / 60;
-    const maxY1Position = 95;
-    let rect = document.getElementById('rect2');
-    let speedY1 = 1;
-    let positionY1 = 0;
-    document.getElementById('rect2').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionY1 = positionY1 + speedY1;
-      if (positionY1 > maxY1Position || positionY1 < 0) {
-        speedY1 = speedY1 * (-1);
-      }
-      rect.style.top = positionY1 + 'px';
-    }, refreshRate);
-  }
-  function vObstacle2() {
-    const refreshRate = 1000 / 60;
-    const maxY2Position = 45;
-    let rect = document.getElementById('rect3');
-    let speedY2 = 1;
-    let positionY2 = 0;
-    document.getElementById('rect3').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionY2 = positionY2 + speedY2;
-      if (positionY2 > maxY2Position || positionY2 < 0) {
-        speedY2 = speedY2 * (-1);
-      }
-      rect.style.top = positionY2 + 'px';
-    }, refreshRate);
-  }
-  function vObstacle3() {
-    const refreshRate = 1000 / 60;
-    const maxY3Position = 100;
-    let rect = document.getElementById('rect4');
-    let speedY3 = 1;
-    let positionY3 = 0;
-    document.getElementById('rect4').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionY3 = positionY3 + speedY3;
-      if (positionY3 > maxY3Position || positionY3 < 0) {
-        speedY3 = speedY3 * (-1);
-      }
-      rect.style.top = positionY3 + 'px';
-    }, refreshRate);
-  }
-  function vObstacle4() {
-    const refreshRate = 1000 / 60;
-    const maxXPosition = 45;
-    let rect = document.getElementById('rect0');
-    let speedX = 1;
-    let positionX = 0;
-    document.getElementById('rect0').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionX = positionX + speedX;
-      if (positionX > maxXPosition || positionX < 0) {
-        speedX = speedX * (-1);
-      }
-      rect.style.left = positionX + 'px';
-    }, refreshRate);
-  }
-  function hObstacle1() {
-    const refreshRate = 1000 / 60;
-    const maxYPosition = 105;
-    let rect = document.getElementById('rect1');
-    let speedY = 1;
-    let positionY = 0;
-    document.getElementById('rect1').onmouseover = function() {wall()};
-  
-    window.setInterval(() => {
-      positionY = positionY + speedY;
-      if (positionY > maxYPosition || positionY < 0) {
-        speedY = speedY * (-1);
-      }
-      rect.style.top = positionY + 'px';
-    }, refreshRate);
-  }
   //LEVEL 2
   //LEVEL 3
+  function level3(){
+    window.onload = () => {
+        startSetTimeoutAnimation();
+        startSetTimeoutAnimation2();
+        vObstacle1();
+        vObstacle2();
+        vObstacle3();
+      };
+      // VERTICAL ANIMATION
+      function startSetTimeoutAnimation() {
+        const refreshRate = 1000 / 60;
+        const maxXPosition = 130;
+        let rect = document.getElementById('rect0');
+        let speedX = 1;
+        let positionX = 0;
+        document.getElementById('rect0').onmouseover = function() {wall()};
+      
+        window.setInterval(() => {
+          positionX = positionX + speedX;
+          if (positionX > maxXPosition || positionX < 0) {
+            speedX = speedX * (-1);
+          }
+          rect.style.left = positionX + 'px';
+        }, refreshRate);
+      }
+      // HORIZONTAL ANIMATION
+        function startSetTimeoutAnimation2() {
+        const refreshRate = 1000 / 60;
+        const maxYPosition = 195;
+        let rect = document.getElementById('rect1');
+        let speedY = 0.7;
+        let positionY = 0;
+        document.getElementById('rect1').onmouseover = function() {wall()};
+      
+        window.setInterval(() => {
+          positionY = positionY + speedY;
+          if (positionY > maxYPosition || positionY < 0) {
+            speedY = speedY * (-1);
+          }
+          rect.style.top = positionY + 'px';
+        }, refreshRate);
+      }
+      //LEVEL 1
+      function vObstacle1() {
+        const refreshRate = 1000 / 60;
+        const maxY1Position = 95;
+        let rect = document.getElementById('rect2');
+        let speedY1 = 1;
+        let positionY1 = 0;
+        document.getElementById('rect2').onmouseover = function() {wall()};
+      
+        window.setInterval(() => {
+          positionY1 = positionY1 + speedY1;
+          if (positionY1 > maxY1Position || positionY1 < 0) {
+            speedY1 = speedY1 * (-1);
+          }
+          rect.style.top = positionY1 + 'px';
+        }, refreshRate);
+      }
+      function vObstacle2() {
+        const refreshRate = 1000 / 60;
+        const maxY2Position = 45;
+        let rect = document.getElementById('rect3');
+        let speedY2 = 1;
+        let positionY2 = 0;
+        document.getElementById('rect3').onmouseover = function() {wall()};
+      
+        window.setInterval(() => {
+          positionY2 = positionY2 + speedY2;
+          if (positionY2 > maxY2Position || positionY2 < 0) {
+            speedY2 = speedY2 * (-1);
+          }
+          rect.style.top = positionY2 + 'px';
+        }, refreshRate);
+      }
+      function vObstacle3() {
+        const refreshRate = 1000 / 60;
+        const maxY3Position = 100;
+        let rect = document.getElementById('rect4');
+        let speedY3 = 1;
+        let positionY3 = 0;
+        document.getElementById('rect4').onmouseover = function() {wall()};
+      
+        window.setInterval(() => {
+          positionY3 = positionY3 + speedY3;
+          if (positionY3 > maxY3Position || positionY3 < 0) {
+            speedY3 = speedY3 * (-1);
+          }
+          rect.style.top = positionY3 + 'px';
+        }, refreshRate);
+      }
+    
+      }
+      
